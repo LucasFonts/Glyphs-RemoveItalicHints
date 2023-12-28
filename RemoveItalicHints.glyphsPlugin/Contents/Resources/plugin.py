6e0f05e366d72c96ebc714651d403cbf8a89ef5e
Jens Kutilek <webmail@kutilek.de>
2023-12-28 13:04:57 +0100

    Implement logic

--- RemoveItalicHints.glyphsPlugin/Contents/Resources/plugin.py
+++ RemoveItalicHints.glyphsPlugin/Contents/Resources/plugin.py
@@ -2,8 +2,13 @@ from __future__ import annotations
 
 import objc
 
-from GlyphsApp import Glyphs, GSCallbackHandler
+from AppKit import NSError
+from GlyphsApp import Glyphs, GSCallbackHandler, STEM
 from GlyphsApp.plugins import GeneralPlugin
+from typing import TYPE_CHECKING, Tuple
+
+if TYPE_CHECKING:
+    from GlyphsApp import GSGlyph, GSLayer
 
 
 class RemoveItalicHints(GeneralPlugin):
@@ -21,12 +26,27 @@ class RemoveItalicHints(GeneralPlugin):
 
     @objc.typedSelector(b"c32@:@@@o^@")
     def interpolateLayer_glyph_interpolation_error_(
-        self, layer, glyph, interpolation, error
-    ):
-        print("__interpolateLayer", layer, glyph, interpolation)
-        # if error:
-        #     return False, NSError()
-        return True, None
+        self, layer: GSLayer, glyph: GSGlyph, interpolation, error
+    ) -> Tuple[bool, None | NSError]:
+        success = True
+        err = None
+
+        # Interpolate the italic angle
+        angle = 0
+        for master_id, factor in interpolation.items():
+            angle += factor * glyph.parent.masters[master_id].italicAngle
+
+        # If the font is italic, delete vertical hints
+        if abs(angle) > 0.1:
+            for i in reversed(range(len(layer.hints))):
+                hint = layer.hints[i]
+                if not hint.isPostScript:
+                    continue
+
+                if not hint.horizontal and hint.type == STEM:
+                    del layer.hints[i]
+
+        return success, err
 
     @objc.python_method
     def __file__(self):
